247aff60dbf5aa97d3e4ab22968599bb3e053efa
JinHyeok Park <jhp@example.com>
2019-11-03 20:19:39 +0900

    added serializers, but still need to refactor views

--- campus_map/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class CampusMapConfig(AppConfig):
-    name = 'campus_map'
--- campus_map/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class Campus(models.Model):
-    university = models.OneToOneField(
-        'account.University',
-        on_delete=models.CASCADE,
-    )
-
